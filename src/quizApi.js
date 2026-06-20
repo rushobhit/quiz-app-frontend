423397393e7690d60752ef391950858df1379f51
Shobhit Awasthi <138900922+rushobhit@users.noreply.github.com>
2026-06-20 16:59:09 +0530

    Deploying on render

--- src/quizApi.js
+++ src/quizApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearAuth } from "./utils/authStorage";
 
 // For LOCAL development
-const API_BASE ="https://quiz-app-backend-production-53ee.up.railway.app";
+const API_BASE ="https://quiz-app-backend-sus3.onrender.com";
   const api = axios.create({
   baseURL: API_BASE,
   headers: {
